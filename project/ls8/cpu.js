73b175e41c393f9d3489fc1874360ba5f7188654
unknown <luisd824@gmail.com>
2018-05-15 15:38:37 -0400

    got 8 to log

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -2,6 +2,9 @@
  * LS-8 v2.0 emulator skeleton code
  */
 
+const LDI = 0b10011001;
+const PRN = 0b01000011;
+const HLT = 0b00000001;
 /**
  * Class for simulating a simple Computer (CPU & memory)
  */
@@ -52,20 +55,19 @@ class CPU {
    * op can be: ADD SUB MUL DIV INC DEC CMP
    */
   alu(op, regA, regB) {
-    switch (op) {
-      case 'MUL':
-        // !!! IMPLEMENT ME
-        return (regA *= regB);
-        break;
-      case 'ADD':
-        return (regA += regB);
-        break;
-      case 'SUB':
-        return (regA -= regB);
-        break;
-      case 'DIV':
-        return regA / regB;
-    }
+    // switch (op) {
+    //   case 'MUL':
+    //     return (regA *= regB);
+    //     break;
+    //   case 'ADD':
+    //     return (regA += regB);
+    //     break;
+    //   case 'SUB':
+    //     return (regA -= regB);
+    //     break;
+    //   case 'DIV':
+    //     return regA / regB;
+    // }
   }
 
   /**
@@ -76,20 +78,46 @@ class CPU {
     // from the memory address pointed to by the PC. (I.e. the PC holds the
     // index into memory of the instruction that's about to be executed
     // right now.)
-    // !!! IMPLEMENT ME
+
+    const IR = this.ram.read(this.PC);
+
     // Debugging output
-    //console.log(`${this.PC}: ${IR.toString(2)}`);
+    console.log(`${this.PC}: ${IR.toString(2)}`);
+
     // Get the two bytes in memory _after_ the PC in case the instruction
     // needs them.
-    // !!! IMPLEMENT ME
+
+    const operandA = this.ram.read(this.PC + 1);
+    const operandB = this.ram.read(this.PC + 2);
+
     // Execute the instruction. Perform the actions for the instruction as
     // outlined in the LS-8 spec.
-    // !!! IMPLEMENT ME
+    switch (IR) {
+      case LDI:
+        this.reg[operandA] = operandB;
+        this.PC += 3;
+        break;
+
+      case PRN:
+        console.log(this.reg[operandA]);
+        this.PC += 2;
+        break;
+
+      case HLT:
+        this.stopClock();
+        break;
+
+      default:
+        console.log(`Unknown instruction ${this.PC}: ${IR.toString(2)}`);
+        this.stopClock();
+    }
     // Increment the PC register to go to the next instruction. Instructions
     // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
     // instruction byte tells you how many bytes follow the instruction byte
     // for any particular instruction.
     // !!! IMPLEMENT ME
+
+    // this.PC += (IR >> 6) + 1;
   }
 }
 
